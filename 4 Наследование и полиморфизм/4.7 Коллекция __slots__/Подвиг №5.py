8daef1cc95db2094225ea13061f4c67d02a167ac
Андрей <107322869+Grimorum1@users.noreply.github.com>
2022-12-12 23:07:54 +0300

    Новые решения

---
+++ 4 Наследование и полиморфизм/4.7 Коллекция __slots__/Подвиг №5.py
@@ -0,0 +1,7 @@
+class Planet:
+
+    def __init__(self, name, diametr, period_solar, period):
+        self._name = name
+        self._diametr = diametr
+        self._period_solar = period_solar
+        self._period = period
